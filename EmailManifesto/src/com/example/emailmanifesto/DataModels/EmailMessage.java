e7bdc2bf0a7325b4346f6fdf3d976fdf329fb041
jbialas <kuba100@operamail.com>
2013-11-20 20:40:08 -0500

    Changed location of EmailMessage and created DataModels folder

---
+++ EmailManifesto/src/com/example/emailmanifesto/DataModels/EmailMessage.java
@@ -0,0 +1,11 @@
+package com.example.emailmanifesto.DataModels;
+
+public class EmailMessage {
+	private String subject;
+	private String author;
+	private String[] recipients;
+	private int priority;
+	private DateTime sentTime;
+	private Message message;
+	
+}
