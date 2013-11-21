3a48b9e7b872efc219fb166bca56252f5368c370
jbialas <kuba100@operamail.com>
2013-11-20 20:35:33 -0500

    Addition of javamail.jar and EmailMessage
    Added javamail.jar to allow for send/receive of emails. Added
    EmailMessage (incomplete).

---
+++ EmailManifesto/src/com/example/emailmanifesto/EmailMessage.java
@@ -0,0 +1,10 @@
+package com.example.emailmanifesto;
+
+public class EmailMessage {
+	private String subject;
+	private String author;
+	private String[] recipients;
+	private int priority;
+	
+	
+}
