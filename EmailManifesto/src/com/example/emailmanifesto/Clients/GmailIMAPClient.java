a3540950b233c8e77c0ed3fe91ef88d15f54e2ed
jbialas <kuba100@operamail.com>
2013-11-26 14:23:12 -0500

    Addition of Gmail IMAP client and support libs
    Added GmailIMAPClient which provides facade for accessing email (not to
    be used directly). Updated activation, additional, and, and mail.jar to
    include SASL libraries. Added Oauth2SaslClient and Factory as support
    classes for IMAP to use.

---
+++ EmailManifesto/src/com/example/emailmanifesto/Clients/GmailIMAPClient.java
@@ -0,0 +1,62 @@
+package com.example.emailmanifesto.Clients;
+
+import java.security.Provider;
+import java.security.Security;
+import java.util.Properties;
+
+import javax.mail.Session;
+import javax.mail.URLName;
+
+import com.example.emailmanifesto.SupportClasses.OAuth2SaslClientFactory;
+import com.sun.mail.imap.IMAPSSLStore;
+import com.sun.mail.imap.IMAPStore;
+
+public class GmailIMAPClient {
+
+	public static final GmailIMAPClient instance = new GmailIMAPClient();
+	public static final String MAIL_SERVER = "imap.gmail.com";
+	public static final int PORT = 993;
+
+	private GmailIMAPClient() {
+		Security.addProvider(new OAuth2Provider());
+	}
+
+	/**
+	 * Connects to the IMAP server with given credentials via OAUTH2
+	 * 
+	 * @param email
+	 * 		user's email
+	 * @param token
+	 * 		OAUTH2 token to authenticate with
+	 * @param debug
+	 * 		enable debugging the connection if true
+	 * @return
+	 * 		IMAP store authenticated with passed in credentials
+	 */
+	public IMAPStore connectToImap(String email, String token, boolean debug)
+			throws Exception {
+		Properties props = new Properties();
+		props.put("mail.imaps.sasl.enable", "true");
+		props.put("mail.imaps.sasl.mechanisms", "XOAUTH2");
+		props.put(OAuth2SaslClientFactory.OAUTH_TOKEN_PROP, token);
+		Session session = Session.getInstance(props);
+		session.setDebug(debug);
+
+		final URLName unusedUrlName = null;
+		IMAPSSLStore store = new IMAPSSLStore(session, unusedUrlName);
+		// empty password forces OAUTH2 login
+		store.connect(MAIL_SERVER, PORT, email, "");
+		return store;
+	}
+
+	public static final class OAuth2Provider extends Provider {
+		private static final long serialVersionUID = -1860612702191596794L;
+
+		public OAuth2Provider() {
+			super("Google OAuth2 Provider", 1.0,
+					"Provides the XOAUTH2 SASL Mechanism");
+			put("SaslClientFactory.XOAUTH2",
+					"com.jbialas.emailtestapp.OAuth2SaslClientFactory");
+		}
+	}
+}
--- EmailManifesto/src/com/example/emailmanifesto/Clients/GmailSMTPClient.java
+++ EmailManifesto/src/com/example/emailmanifesto/Clients/GmailSMTPClient.java
@@ -9,63 +9,65 @@ import javax.mail.URLName;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.util.ByteArrayDataSource;
-
 import com.sun.mail.smtp.SMTPTransport;
 import com.sun.mail.util.BASE64EncoderStream;
 
 public class GmailSMTPClient {
 	private Session session;
-	
+
+	public static final String MAIL_SERVER = "smtp.gmail.com";
+	public static final int PORT = 587;
+
 	public static final GmailSMTPClient instance = new GmailSMTPClient();
-	
-	private GmailSMTPClient(){
-		
+
+	private GmailSMTPClient() {
+
 	}
-	
-	private SMTPTransport connectToSMTP(String host, int port, String userEmail,
-	        String oauthToken, boolean debug) throws Exception {
 
-	    Properties props = new Properties();
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.smtp.starttls.required", "true");
-	    props.put("mail.smtp.sasl.enable", "false");
-	    session = Session.getInstance(props);
-	    session.setDebug(debug);
+	private SMTPTransport connectToSMTP(String host, int port,
+			String userEmail, String oauthToken, boolean debug)
+			throws Exception {
 
+		Properties props = new Properties();
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.starttls.required", "true");
+		props.put("mail.smtp.sasl.enable", "false");
+		session = Session.getInstance(props);
+		session.setDebug(debug);
 
-	    final URLName unusedUrlName = null;
-	    SMTPTransport transport = new SMTPTransport(session, unusedUrlName);
-	    // If the password is non-null, SMTP tries to do AUTH LOGIN.
-	    final String emptyPassword = null;
-	    transport.connect(host, port, userEmail, emptyPassword);
+		final URLName unusedUrlName = null;
+		SMTPTransport transport = new SMTPTransport(session, unusedUrlName);
+		// null password forces OAUTH2 login
+		transport.connect(host, port, userEmail, null);
 
-	            byte[] response = String.format("user=%s\1auth=Bearer %s\1\1", userEmail,
-	            oauthToken).getBytes();
-	    response = BASE64EncoderStream.encode(response);
+		byte[] response = String.format("user=%s\1auth=Bearer %s\1\1",
+				userEmail, oauthToken).getBytes();
+		response = BASE64EncoderStream.encode(response);
 
-	    transport.issueCommand("AUTH XOAUTH2 " + new String(response),
-	            235);
+		transport.issueCommand("AUTH XOAUTH2 " + new String(response), 235);
 
-	    return transport;
+		return transport;
 	}
-	
+
 	public synchronized void sendMail(String subject, String body, String user,
-	        String oauthToken, String recipients) throws Exception {
-	        SMTPTransport smtpTransport = connectToSMTP("smtp.gmail.com",
-	                587,
-	                user,
-	                oauthToken,
-	                true);
+			String oauthToken, String recipients) throws Exception {
+		SMTPTransport smtpTransport = connectToSMTP(MAIL_SERVER, PORT, user,
+				oauthToken, true);
 
-	        MimeMessage message = new MimeMessage(session);
-	        DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));   
-	                message.setSender(new InternetAddress(user));   
-	                message.setSubject(subject);   
-	                message.setDataHandler(handler);   
-	        if (recipients.indexOf(',') > 0)   
-	            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));   
-	        else  
-	            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));   
-	        smtpTransport.sendMessage(message, message.getAllRecipients());   
+		MimeMessage message = new MimeMessage(session);
+		DataHandler handler = new DataHandler(new ByteArrayDataSource(
+				body.getBytes(), "text/plain"));
+		message.setSender(new InternetAddress(user));
+		message.setSubject(subject);
+		message.setDataHandler(handler);
+
+		//if more than one recipient, parse it as such
+		if (recipients.indexOf(',') > 0)
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(recipients));
+		else
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(
+					recipients));
+		smtpTransport.sendMessage(message, message.getAllRecipients());
 	}
 }
